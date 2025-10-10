1c5b455338623fa5ec1c45a7a0a9b042502acb0c
Yash-K18 <124431842+22f3002322@users.noreply.github.com>
2025-10-11 01:07:08 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import TeacherDashboard from "./components/TeacherDashboard";
+import StudentDashboard from "./components/StudentDashboard";
+import RoleSelection from "./components/RoleSelection";
+import StudentStart from "./components/StudentStart";
+import WaitingScreen from "./components/WaitingScreen";
+import StudentResults from "./components/StudentResults";
+import ResultsWithChat from "./components/ResultsWithChat";
+import KickedOut from "./components/KickedOut";
+
+import TeacherCreate from './components/teacher/TeacherCreate';
+import TeacherLive from './components/teacher/TeacherLive';
+import TeacherHistory from './components/teacher/TeacherHistory';
+
+
+import { SocketContext, socket } from "./context/SocketContext";
+
+function App() {
+  return (
+    <SocketContext.Provider value={socket}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RoleSelection />} />
+          <Route path="/student/start" element={<StudentStart />} />
+          <Route path="/student/wait" element={<WaitingScreen />} />
+          <Route path="/teacher" element={<TeacherDashboard />} />
+          <Route path="/student" element={<StudentDashboard />} />
+          <Route path="/student/results" element={<StudentResults />} />
+          <Route path="/student results-chat" element={<ResultsWithChat />} />
+          <Route path="/kicked" element={<KickedOut />} />
+
+          <Route path="/teacher" element={<RoleSelection />} />
+          <Route path="/teacher/create" element={<TeacherCreate />} />
+          <Route path="/teacher/live" element={<TeacherLive />} />
+          <Route path="/teacher/history" element={<TeacherHistory />} />
+        </Routes>
+      </BrowserRouter>
+    </SocketContext.Provider>
+  );
+}
+
+export default App;
