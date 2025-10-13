4f85644040562cd47b462d3d2f25281ece6bef2a
Yash-K18 <124431842+22f3002322@users.noreply.github.com>
2025-10-13 12:09:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const FRONTEND_URL = process.env.FRONTEND_URL || [
   "http://localhost:3000",
-  "https://interpoll-live.vercel.app/",
+  "https://interpoll-live.vercel.app",
 ];
 
 const app = express();
@@ -135,6 +135,7 @@ app.get("/history", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
